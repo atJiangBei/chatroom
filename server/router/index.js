0d2f5d51003022954786a48bf74407867727de6b
JiangBei <engineersweb@163.com>
2021-08-24 00:17:27 +0800

    update

--- server/router/index.js
+++ server/router/index.js
@@ -3,37 +3,39 @@ let {
   findDataFromName,
   getJsonData,
   updateJsonData,
-  createRandomStr,
+  createRandomStr
 } = require('./../utils')
 
-router.get('/chat/queryAllUser', async (ctx) => {
+router.get('/chat/queryAllUser', async ctx => {
   try {
     const { id } = ctx.query
     const userPath = './datajson/user.json'
     const jsondata = await getJsonData(userPath)
-    const index = jsondata.findIndex((res) => res.id === id)
-    jsondata.splice(index, 1)
+    const index = jsondata.findIndex(res => res.id === id)
+    if (index > -1) {
+      jsondata.splice(index, 1)
+    }
     ctx.body = {
       code: 0,
       msg: '成功',
-      data: jsondata,
+      data: jsondata
     }
   } catch (error) {
     ctx.body = {
       code: 2,
-      msg: error,
+      msg: error
     }
   }
 })
 
-router.get('/chat/register', async (ctx) => {
+router.get('/chat/register', async ctx => {
   console.log(ctx.query)
   try {
     const { name, password } = ctx.query
     if (!name || !password) {
       ctx.body = {
         code: 1,
-        msg: '参数错误！用户名或密码为必填项',
+        msg: '参数错误！用户名或密码为必填项'
       }
       return
     }
@@ -43,37 +45,37 @@ router.get('/chat/register', async (ctx) => {
     if (data) {
       ctx.body = {
         code: 1,
-        msg: '用户名重复',
+        msg: '用户名重复'
       }
     } else {
       const userData = {
         name,
         password,
         createTime: Date.now(),
-        id: createRandomStr(),
+        id: createRandomStr()
       }
       jsondata.push(userData)
       await updateJsonData(userPath, JSON.stringify(jsondata))
       ctx.body = {
         code: 0,
-        msg: '注册成功',
+        msg: '注册成功'
       }
     }
   } catch (error) {
     ctx.body = {
       code: 2,
-      msg: error,
+      msg: error
     }
   }
 })
 
-router.get('/chat/login', async (ctx) => {
+router.get('/chat/login', async ctx => {
   try {
     const { name, password } = ctx.query
     if (!name || !password) {
       ctx.body = {
         code: 1,
-        msg: '参数错误！用户名或密码为必填项',
+        msg: '参数错误！用户名或密码为必填项'
       }
       return
     }
@@ -81,21 +83,28 @@ router.get('/chat/login', async (ctx) => {
     const jsondata = await getJsonData(userPath)
     const data = await findDataFromName(jsondata, name)
     if (data) {
-      ctx.body = {
-        code: 0,
-        msg: '成功',
-        data,
+      if (data.password === password) {
+        ctx.body = {
+          code: 0,
+          msg: '成功',
+          data
+        }
+      } else {
+        ctx.body = {
+          code: 1,
+          msg: '密码错误'
+        }
       }
     } else {
       ctx.body = {
         code: 1,
-        msg: '无此用户',
+        msg: '无此用户'
       }
     }
   } catch (error) {
     ctx.body = {
       code: 2,
-      msg: error,
+      msg: error
     }
   }
 })
--- web/src/pages/chat/index.tsx
+++ web/src/pages/chat/index.tsx
@@ -1,62 +1,112 @@
 import React, { useEffect, useState } from 'react'
+import { Input } from 'antd'
 import './index.scss'
 import request from '../../utils/request'
-import io from "socket.io-client"
+import io from 'socket.io-client'
 const userInfoStr = localStorage.getItem('userInfo')
-const userInfo = JSON.parse(userInfoStr || JSON.stringify({}))
-const socket = io(`ws://${process.env.REACT_APP_WS}`, {
-    query: {
-        name: "other"
-    },
-    auth: {
-        ...userInfo
+let userInfo: any = {}
+let socket: any = null
+const chatData: any = {}
+const Chat = (props: any) => {
+  if (userInfoStr) {
+    userInfo = JSON.parse(userInfoStr)
+  } else {
+    props.history.push('/login')
+  }
+  const [userList, setUserList] = useState([])
+  const [currentUser, setCurrentUser] = useState<any>()
+  const [currentChat, setCurrentChat] = useState<any>()
+  useEffect(() => {
+    if (userInfo) {
+      queryAllUser(userInfo)
+      socket = io(`ws://${process.env.REACT_APP_WS}`, {
+        query: {
+          name: 'other'
+        },
+        auth: {
+          ...userInfo
+        }
+      })
     }
-});
 
-const Chat = () => {
-    const [userList, setUserList] = useState([])
-    const [currentUser, setCurrentUser] = useState < any > ()
-    useEffect(() => {
-        request.GET("/chat/queryAllUser", {
-            params: userInfo
-        }).then((res: any) => {
-            setUserList(res)
+    socket.on('connect', () => {
+      console.log('连接成功')
+    })
+    socket.on('msg', (res: any) => {
+      console.log(res)
+      console.log(currentChat)
+      const { id } = res.from
+      chatData[id].push(res.message)
+      setCurrentChat([...chatData[id]])
+    })
+  }, [])
+  const queryAllUser = (userInfo = {}) => {
+    request
+      .GET('/chat/queryAllUser', {
+        params: userInfo
+      })
+      .then((res: any) => {
+        setUserList(res)
+        res.forEach((item: any) => {
+          chatData[item.id] = []
         })
-        socket.on("connect", () => {
-            console.log("连接成功")
-        });
-        socket.on("msg", (res: any) => {
-            console.log(res)
-        });
-    }, [])
-    const toggleUser = (user: any) => {
-        socket.emit("msg", {
-            type: 'fellow_netizen',
-            to: user
-        });
-        setCurrentUser(user)
-    }
-    return <div className="chat-container">
-        <div className="chat-content flex">
-            <div className="user-self">
-                <h1>{userInfo.name}</h1>
-            </div>
-            <div className="user-list">
-                <ul>
-                    {
-                        userList.length &&
-                        userList.map((user: any) => {
-                            const cs = currentUser && currentUser.id === user.id
-                            return <li key={user.id} className={cs ? 'active' : ''} onClick={() => toggleUser(user)}>{user.name}</li>
-                        })
-                    }
-                </ul>
-            </div>
-            <div className="flex-1">
-
-            </div>
+      })
+  }
+  const toggleUser = (user: any) => {
+    setCurrentUser(user)
+    setCurrentChat(chatData[user.id])
+  }
+  const onPressEnter = function(e: any) {
+    const value = e.target.value
+    socket.emit('msg', {
+      type: 'fellow_netizen',
+      to: currentUser,
+      message: value
+    })
+  }
+  return (
+    <div className="chat-container">
+      <div className="chat-content flex">
+        <div className="user-self">
+          <h1>{userInfo.name}</h1>
         </div>
+        <div className="user-list">
+          <ul>
+            {userList.length &&
+              userList.map((user: any) => {
+                const cs = currentUser && currentUser.id === user.id
+                return (
+                  <li
+                    key={user.id}
+                    className={cs ? 'active' : ''}
+                    onClick={() => toggleUser(user)}
+                  >
+                    {user.name}
+                  </li>
+                )
+              })}
+          </ul>
+        </div>
+        {currentUser && (
+          <div className="flex-1 flex chat-interface fd-column">
+            <div className="flex-1">
+              <ul>
+                {currentChat.map((chat: any) => {
+                  return <li key={chat}>{chat}</li>
+                })}
+              </ul>
+            </div>
+            <div className="chat-input-box padding-default">
+              <Input.TextArea
+                placeholder="按enter+ctrl发送"
+                onPressEnter={onPressEnter}
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
+  )
 }
 
 export default Chat
